53cfd2b5b128c629e217c862ba86c5372717d45d
fe-one <2460265235@qq.com>
2019-09-18 11:08:28 +0800

    Update CountryCell.swift

--- CountryList/CountryCell.swift
+++ CountryList/CountryCell.swift
@@ -22,7 +22,7 @@ class CountryCell: UITableViewCell {
         }
     }
     
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setup()
         
